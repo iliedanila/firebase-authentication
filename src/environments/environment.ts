b96d6d3ea09f10c57f1acac1ecf8dd8213c0167a
Ilie Danila <ilie.danila@proton.me>
2023-02-26 23:43:13 +0200

    firebase authentication

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyABQWfnKaymhonKcqVE2CzrZeBJjqNrK7w",
+        authDomain: "fir-authentication-9e615.firebaseapp.com",
+        projectId: "fir-authentication-9e615",
+        storageBucket: "fir-authentication-9e615.appspot.com",
+        messagingSenderId: "519648237350",
+        appId: "1:519648237350:web:d9b009cc4a4ef98fc1d3e7",
+        measurementId: "G-EKNL7Z7NB0",
+    },
+};
